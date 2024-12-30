5de6179022add285068e8beada899b460acd169a
Milk-Cool <milan-pl@mail.ru>
2024-12-30 19:02:43 +0300

    add position module

---
+++ builtin/pos.js
@@ -0,0 +1,19 @@
+export default function posPlugin(from, packetID, data, Packet, meta) {
+    if(from === "client") {
+        if(packetID !== 0x1c) return;
+        // position
+        const x = data.readDoubleBE(0);
+        const y = data.readDoubleBE(8);
+        const z = data.readDoubleBE(16);
+        console.log("pos update from client", meta.username, x, y, z);
+    } else if(from === "server") {
+        if(packetID !== 0x42) return;
+        // position but from server
+        let _;
+        [_, data] = Packet.readVarInt(data);
+        const x = data.readDoubleBE(0);
+        const y = data.readDoubleBE(8);
+        const z = data.readDoubleBE(16);
+        console.log("pos update from server", meta.username, x, y, z);
+    }
+}
